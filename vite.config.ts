682146505ebdaa3461f9c26ec2ca2e25e4ed2fa2
AryaBaride <baridearya@gmail.com>
2025-09-08 16:13:24 +0530

    Push updated config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   // Set the base path directly for GitHub Pages.
-  base: "/pipeline-portrait/",
+  base: "./",
   plugins: [
     react(),
   ],
